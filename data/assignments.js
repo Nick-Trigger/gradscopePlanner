a08b265698ce0955df663d417df76e46b24301ff
planit <planit@tanjeffreyz.github.io>
2024-08-26 19:34:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,224 +1,115 @@
 assignments = {
-  "BME 221L.001.Sp24": [
+  "BME 271D.001.Fa24": [
     {
-      "title": "Homework #0",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-01-19T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/3871917/submissions/222621905",
-      "submitted": true
-    },
-    {
-      "title": "Homework #1",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-01-26T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/3871921/submissions/225100558",
-      "submitted": true
-    },
-    {
-      "title": "Homework #2",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-02-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/3871939/submissions/227167043",
-      "submitted": true
-    },
-    {
-      "title": "Homework #3",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-02-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/3901028/submissions/230126304",
-      "submitted": true
-    },
-    {
-      "title": "Homework #4",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-02-23T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/3901037/submissions/233194093",
-      "submitted": true
-    },
-    {
-      "title": "Homework #5",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-03-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/4095494/submissions/235855994",
-      "submitted": true
-    },
-    {
-      "title": "Homework #6",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-03-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/4095495/submissions/237872969",
-      "submitted": true
-    },
-    {
-      "title": "Homework #7",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-03-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/4095496/submissions/241281312",
-      "submitted": true
-    },
-    {
-      "title": "Homework #8",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-03-29T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/4095498/submissions/242894062",
-      "submitted": true
-    },
-    {
-      "title": "Homework #9",
-      "course": "BME 221L.001.Sp24",
-      "dueDate": "2024-04-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/693383/assignments/4095499/submissions/246387029",
-      "submitted": true
-    }
-  ],
-  "BME 244L.001.Sp24": [
-    {
-      "title": "HW1",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-01-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/3957092/submissions/224485146",
-      "submitted": true
-    },
-    {
-      "title": "HW2",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-02-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4011791/submissions/226444584",
-      "submitted": true
-    },
-    {
-      "title": "HW3",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-02-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4045708/submissions/229437903",
-      "submitted": true
-    },
-    {
-      "title": "Lab Report 1 - Nerve Conduction Velocity",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-02-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4051290/submissions/231693394",
-      "submitted": true
-    },
-    {
-      "title": "Lab Report 2 - Muscle Contraction",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-03-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4051291/submissions/237682663",
-      "submitted": true
-    },
-    {
-      "title": "Lab Report 3 - Pulse Wave Velocity",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-04-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4051293/submissions/243645047",
-      "submitted": true
-    },
-    {
-      "title": "Lab Report 4 - Muscle Fatigue",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-04-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943",
+      "title": "Discussion Worksheet 2A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
     },
     {
-      "title": "Stat Module 3 Post-Quiz (1:25 Section)",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-02-14T16:30:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4103632/submissions/230717897",
-      "submitted": true
-    },
-    {
-      "title": "HW4",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-03-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4143193/submissions/237563028",
-      "submitted": true
-    },
-    {
-      "title": "HW5",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-03-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4235779/submissions/241263518",
-      "submitted": true
-    },
-    {
-      "title": "HW6",
-      "course": "BME 244L.001.Sp24",
-      "dueDate": "2024-04-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/713943/assignments/4322815/submissions/248763377",
-      "submitted": true
-    }
-  ],
-  "Math 353": [
-    {
-      "title": "Homework One FULL",
-      "course": "Math 353",
-      "dueDate": "2024-01-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/3951089/submissions/223930899",
-      "submitted": true
-    },
-    {
-      "title": "Homework 2",
-      "course": "Math 353",
-      "dueDate": "2024-01-29T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4013978/submissions/225710508",
-      "submitted": true
-    },
-    {
-      "title": "Homework 3",
-      "course": "Math 353",
-      "dueDate": "2024-02-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4038562/submissions/227923230",
-      "submitted": true
-    },
-    {
-      "title": "Homework 4",
-      "course": "Math 353",
-      "dueDate": "2024-02-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4079564/submissions/230166022",
-      "submitted": true
-    },
-    {
-      "title": "Homework 5",
-      "course": "Math 353",
-      "dueDate": "2024-02-26T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4154472/submissions/234506699",
-      "submitted": true
-    },
-    {
-      "title": "Homework 6",
-      "course": "Math 353",
-      "dueDate": "2024-03-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4176592/submissions/236593537",
-      "submitted": true
-    },
-    {
-      "title": "Homework 7",
-      "course": "Math 353",
-      "dueDate": "2024-03-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4207931/submissions/238299464",
-      "submitted": true
-    },
-    {
-      "title": "Homework 8",
-      "course": "Math 353",
-      "dueDate": "2024-03-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436",
+      "title": "Discussion Worksheet 2B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
     },
     {
-      "title": "Homework 9",
-      "course": "Math 353",
-      "dueDate": "2024-04-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436/assignments/4326641/submissions/244963637",
-      "submitted": true
+      "title": "Discussion Worksheet 3A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
     },
     {
-      "title": "Homework 10",
-      "course": "Math 353",
-      "dueDate": "2024-04-15T23:59:00",
-      "link": "https://www.gradescope.com/courses/711436",
+      "title": "Discussion Worksheet 3B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 4A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 4B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 5A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 5B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 6A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 6B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 7A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 7B",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-10-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 8",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-11-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 9",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-11-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 10",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-11-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
+      "title": "Discussion Worksheet 11",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-12-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
     }
   ]
