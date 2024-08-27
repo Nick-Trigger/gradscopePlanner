f1fc695027150d01c87ed09535fea51640c96d75
planit <planit@tanjeffreyz.github.io>
2024-08-27 18:39:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,8 +4,8 @@ assignments = {
       "title": "Homework #0",
       "course": "BME 260L.001.Fa24",
       "dueDate": "2024-08-29T08:30:00",
-      "link": "https://www.gradescope.com/courses/784178/assignments/4802519/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/784178/assignments/4802519/submissions/262730167",
+      "submitted": true
     }
   ],
   "BME 271D.001.Fa24": [
