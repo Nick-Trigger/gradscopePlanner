272f3a1d8f1cb8fcf075859cf69e497ba58a2da0
planit <planit@tanjeffreyz.github.io>
2024-09-04 11:34:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -31,6 +31,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Discussion Worksheet 2A",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
+    },
+    {
       "title": "Discussion Worksheet 2B",
       "course": "BME 271D.001.Fa24",
       "dueDate": "2024-09-18T23:59:00",
