31b81c3a4adda8c2682e5ae2d2654d1c5a6e80e2
planit <planit@tanjeffreyz.github.io>
2024-10-28 11:36:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -183,6 +183,13 @@ assignments = {
       "dueDate": "2024-12-08T23:59:00",
       "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
+    },
+    {
+      "title": "Final Project Proposal",
+      "course": "BME 271D.001.Fa24",
+      "dueDate": "2024-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/834695",
+      "submitted": false
     }
   ],
   "CHEM 201DL.002.F24": []
