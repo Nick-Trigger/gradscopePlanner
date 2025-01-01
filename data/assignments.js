0744e74bdfa6e0df6018ee9a0f97bc185c432248
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:09:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,133 +3,133 @@ assignments = {
     {
       "title": "Homework #0",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-08-29T08:30:00",
+      "dueDate": "2025-08-29T08:30:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4802519/submissions/262730167",
       "submitted": true
     },
     {
       "title": "Homework #1",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-04T12:00:00",
+      "dueDate": "2025-09-04T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4848046/submissions/264192015",
       "submitted": true
     },
     {
       "title": "Homework #2",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-06T12:00:00",
+      "dueDate": "2025-09-06T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4873620/submissions/264893668",
       "submitted": true
     },
     {
       "title": "Homework #3",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-10T12:00:00",
+      "dueDate": "2025-09-10T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4893435/submissions/265780471",
       "submitted": true
     },
     {
       "title": "Homework #4",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-18T17:00:00",
+      "dueDate": "2025-09-18T17:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4954570/submissions/273352006",
       "submitted": true
     },
     {
       "title": "Mastery Problem #1",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-19T10:15:00",
+      "dueDate": "2025-09-19T10:15:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4982766/submissions/269074152",
       "submitted": true
     },
     {
       "title": "Homework #5",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-20T17:00:00",
+      "dueDate": "2025-09-20T17:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/4986732/submissions/269676164",
       "submitted": true
     },
     {
       "title": "Homework #6",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-09-27T12:00:00",
+      "dueDate": "2025-09-27T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5030707/submissions/271940384",
       "submitted": true
     },
     {
       "title": "Homework #7",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-10-08T12:00:00",
+      "dueDate": "2025-10-08T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5081063/submissions/275687267",
       "submitted": true
     },
     {
       "title": "Mastery Problem #2",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-10-10T10:00:00",
+      "dueDate": "2025-10-10T10:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5135957/submissions/276478246",
       "submitted": true
     },
     {
       "title": "Homework #8",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-01T12:00:00",
+      "dueDate": "2025-11-01T12:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5238341/submissions/284488111",
       "submitted": true
     },
     {
       "title": "Homework #9",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-05T17:00:00",
+      "dueDate": "2025-11-05T17:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5252883/submissions/285702669",
       "submitted": true
     },
     {
       "title": "Homework #10",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-08T17:00:00",
+      "dueDate": "2025-11-08T17:00:00",
       "link": "https://www.gradescope.com/courses/784178",
       "submitted": false
     },
     {
       "title": "Mastery Problem #3",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-12T10:00:00",
+      "dueDate": "2025-11-12T10:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5309920/submissions/288116211",
       "submitted": true
     },
     {
       "title": "Homework #11",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-19T17:00:00",
+      "dueDate": "2025-11-19T17:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5325021/submissions/290771098",
       "submitted": true
     },
     {
       "title": "Homework #12",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-22T17:00:00",
+      "dueDate": "2025-11-22T17:00:00",
       "link": "https://www.gradescope.com/courses/784178",
       "submitted": false
     },
     {
       "title": "Mastery Problem #4 - Timed",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-11-26T23:59:00",
+      "dueDate": "2025-11-26T23:59:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5364389/submissions/292463583",
       "submitted": true
     },
     {
       "title": "Homework #13",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-12-06T17:00:00",
+      "dueDate": "2025-12-06T17:00:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5399228/submissions/295293344",
       "submitted": true
     },
     {
       "title": "Mastery Problem #5",
       "course": "BME 260L.001.Fa24",
-      "dueDate": "2024-12-13T17:01:00",
+      "dueDate": "2025-12-13T17:01:00",
       "link": "https://www.gradescope.com/courses/784178/assignments/5426424/submissions/297808567",
       "submitted": true
     }
@@ -138,147 +138,147 @@ assignments = {
     {
       "title": "Discussion Worksheet 1",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-06T23:59:00",
+      "dueDate": "2025-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784897/submissions/265016543",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 2A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-13T23:59:00",
+      "dueDate": "2025-09-13T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784899/submissions/267261156",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 2B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784901/submissions/267935866",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 3A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-20T23:59:00",
+      "dueDate": "2025-09-20T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784903/submissions/269820447",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 3B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784906/submissions/270634598",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 4A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-27T23:59:00",
+      "dueDate": "2025-09-27T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784909/submissions/272286942",
       "submitted": true
     },
     {
       "title": "Discussion 4B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784911/submissions/273020871",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 5A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-04T23:59:00",
+      "dueDate": "2025-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784912/submissions/274795151",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 5B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-11T23:59:00",
+      "dueDate": "2025-10-11T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784914/submissions/276937970",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 6A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2025-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784916/submissions/279604996",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 6B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784917/submissions/280540705",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 7A",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-25T23:59:00",
+      "dueDate": "2025-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784918/submissions/282175875",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 7B",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784921/submissions/283068094",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 8",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
     },
     {
       "title": "Discussion Worksheet 9",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-11T23:59:00",
+      "dueDate": "2025-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784924/submissions/288016235",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 10",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-21T23:59:00",
+      "dueDate": "2025-11-21T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/4784925/submissions/292128081",
       "submitted": true
     },
     {
       "title": "Discussion Worksheet 11",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-12-07T23:59:00",
+      "dueDate": "2025-12-07T23:59:00",
       "link": "https://www.gradescope.com/courses/834695",
       "submitted": false
     },
     {
       "title": "Final Project Proposal",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/5227594/submissions/285568460",
       "submitted": true
     },
     {
       "title": "Final Project Progress Report",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/5309996/submissions/290540135",
       "submitted": true
     },
     {
       "title": "Redo Exam 2",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-11-26T23:59:00",
+      "dueDate": "2025-11-26T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/5369397/submissions/293006099",
       "submitted": true
     },
     {
       "title": "Final Project Submission",
       "course": "BME 271D.001.Fa24",
-      "dueDate": "2024-12-03T23:59:00",
+      "dueDate": "2025-12-03T23:59:00",
       "link": "https://www.gradescope.com/courses/834695/assignments/5392442/submissions/294369994",
       "submitted": true
     }
