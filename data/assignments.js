422802db786ab343e4435b4c7b29065b5195f4fd
planit <planit@tanjeffreyz.github.io>
2025-01-09 18:43:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -8,5 +8,14 @@ assignments = {
       "submitted": false
     }
   ],
-  "BME 303L.001.Sp25": []
+  "BME 303L.001.Sp25": [],
+  "BME 354L.001.Sp25": [
+    {
+      "title": "I.1 - Pre-Lab",
+      "course": "BME 354L.001.Sp25",
+      "dueDate": "2025-01-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/935264",
+      "submitted": false
+    }
+  ]
 };
