8d4160167ca263a82beabe549841b9bbd76ae87f
planit <planit@tanjeffreyz.github.io>
2025-01-09 02:28:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,12 @@
 assignments = {
+  "BME 254L.001.Sp25": [
+    {
+      "title": "Software Installation & Tutorials",
+      "course": "BME 254L.001.Sp25",
+      "dueDate": "2025-01-14T11:45:00",
+      "link": "https://www.gradescope.com/courses/941961/assignments/5544754/submissions/new",
+      "submitted": false
+    }
+  ],
   "BME 303L.001.Sp25": []
 };
