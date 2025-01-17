357f260b8858ac8dfcb5b7fd8ca52eea2455b59d
planit <planit@tanjeffreyz.github.io>
2025-01-17 16:40:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -22,6 +22,20 @@ assignments = {
       "dueDate": "2025-01-22T23:59:00",
       "link": "https://www.gradescope.com/courses/945035",
       "submitted": false
+    },
+    {
+      "title": "Homework 1A",
+      "course": "BME 303L.001.Sp25",
+      "dueDate": "2025-01-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/945035",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1B",
+      "course": "BME 303L.001.Sp25",
+      "dueDate": "2025-02-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/945035",
+      "submitted": false
     }
   ],
   "BME 354L.001.Sp25": [
