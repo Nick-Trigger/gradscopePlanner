8ce486c7d070248ffe7a8520aff11ad8a80ee19f
planit <planit@tanjeffreyz.github.io>
2025-02-14 18:41:36 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -178,6 +178,13 @@ assignments = {
       "dueDate": "2025-02-17T13:25:00",
       "link": "https://www.gradescope.com/courses/935264",
       "submitted": false
+    },
+    {
+      "title": "III - Lab Report",
+      "course": "BME 354L.001.Sp25",
+      "dueDate": "2025-02-24T13:25:00",
+      "link": "https://www.gradescope.com/courses/935264",
+      "submitted": false
     }
   ]
 };
